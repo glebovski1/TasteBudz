651c721ab78aead54a0969e24bc72a815442722a
Oliver Hand <144288342+handop@users.noreply.github.com>
2026-04-01 14:58:41 -0400

    Update Program.cs
    Set the home page and fixed the log-in page link.

--- src/TasteBudz.Web.Mvc/Program.cs
+++ src/TasteBudz.Web.Mvc/Program.cs
@@ -92,7 +92,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.Run();
